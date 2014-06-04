a8ed467930dd2bf23701c065f5818c062a50196c
psytau <rmanninoFoo@gmail.com>
2014-06-04 15:45:25 +0800

    test for podcast-user relation

--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Podcast, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should belong to a user' do
+    @user = FactoryGirl.create(:user)
+    podcast = Podcast.create! :podcast_name => 'Test Podcast' ,:user => @user
+    expect(podcast.user_id).to be(@user.id)
+  end
 end

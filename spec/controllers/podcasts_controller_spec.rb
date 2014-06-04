d597fb31c9c80ce62963be7f2f1ec63e94224bc7
psytau <rmanninoFoo@gmail.com>
2014-06-04 16:38:15 +0800

    added tests for podcast to be completed

--- spec/controllers/podcasts_controller_spec.rb
+++ spec/controllers/podcasts_controller_spec.rb
@@ -22,9 +22,9 @@ RSpec.describe PodcastsController, :type => :controller do
 
   before(:each) do
     @request.env["devise.mapping"] = Devise.mappings[:user]
-    user = FactoryGirl.create(:user)
-    user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
-    sign_in user
+    @test_user = FactoryGirl.create(:user)
+    @test_user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
+    sign_in @test_user
   end
 
   # This should return the minimal set of attributes required to create a valid
@@ -87,6 +87,13 @@ RSpec.describe PodcastsController, :type => :controller do
         post :create, {:podcast => valid_attributes}
         expect(assigns(:podcast)).to be_a(Podcast)
         expect(assigns(:podcast)).to be_persisted
+      end
+
+      it "assigns a current user to podcast" do
+        post :create, {:podcast => valid_attributes}
+        expect(assigns(:podcast).user_id).to be(@test_user.id)
+        # expect(assigns(:podcast)).to be_a(Podcast)
+        # expect(assigns(:podcast)).to be_persisted
       end
 
       it "redirects to the created podcast" do

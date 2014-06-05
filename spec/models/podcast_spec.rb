9545c83b15b94a6b0e40337aafd1a17489943286
psytau <rmanninoFoo@gmail.com>
2014-06-05 23:32:23 +0800

    use FactoryGirl for Podcast model tests

--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe Podcast, :type => :model do
   it 'should belong to a user' do
     @user = FactoryGirl.create(:user)
-    podcast = Podcast.create! :podcast_name => 'Test Podcast' ,:user => @user
+    podcast = FactoryGirl.build(:valid_podcast)
+    podcast.user = @user
+    podcast.save!
+    # podcast = Podcast.create! :podcast_name => 'Test Podcast' ,:user => @user
     expect(podcast.user_id).to be(@user.id)
   end
 end

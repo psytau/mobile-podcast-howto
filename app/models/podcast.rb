2bfbd4ce53a3b02665c2b500aa02c7cb8968a866
psytau <rmanninoFoo@gmail.com>
2014-06-05 23:07:35 +0800

    added validation for Podcast feed

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,3 +1,5 @@
 class Podcast < ActiveRecord::Base
   belongs_to :user
+  # validates :feed, format: { with : /\Ahttp:\/\//}
+  validates :feed, format: { with: /\Ahttp:\/\//}
 end
--- spec/views/podcasts/show.html.erb_spec.rb
+++ spec/views/podcasts/show.html.erb_spec.rb
@@ -13,10 +13,8 @@ RSpec.describe "podcasts/show", :type => :view do
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Slug/)
-    expect(rendered).to match(/Course Name/)
-    expect(rendered).to match(/Podcast Name/)
-    expect(rendered).to match(/Feed/)
-    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/Step 1/)
+    expect(rendered).to match(/Step 2/)
+    expect(rendered).to match(/Step 3/)
   end
 end
